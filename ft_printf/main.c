725d442f0e711542d5a683d45b4979ed35277d19
nalrashd <101207512+nsuae@users.noreply.github.com>
2022-10-29 16:16:56 +0400

    Update main.c

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -15,28 +15,28 @@
 int	main(void)
 {
 	char	*p;
-	int	f = 25;
+	int	n = 25;
 	/*------------This is the built int printf------------*/
 	printf("\n\033[0;36m------------Built in printf------------\033[0m\n\n");
-	printf("The is string %s\n", "Hello my name is Noora Alrashdi");
+	printf("The is string: %s\n", "Hello world");
 	printf("The is decimal: %d\n", '6');
 	printf("The is integer: %i\n", '6');
 	printf("The is unsigned decimal: %u\n", '6');
-	printf("The is lower case hexa: %x\n", f);
-	printf("The is upper case hexa: %X\n", f);
-	printf("The is  pointer: %p\n", p);
-	printf("The is haracter: %c\n", 'f');
+	printf("The is lower case hexa: %x\n", n);
+	printf("The is upper case hexa: %X\n", n);
+	printf("The is pointer: %p\n", p);
+	printf("The is character: %c\n", 'n');
 	printf("The is percentage: %%\n\n");
 	
 	/*------------This is my ft_printf------------*/
 	ft_printf("\n\033[0;32m------------My ft_printf------------\033[0m\n\n");
-	ft_printf("The is string: %s\n", "Hello my name is Noora Alrashdi");
+	ft_printf("The is string: %s\n", "Hello world");
 	ft_printf("The is decimal: %d\n", '6');
 	ft_printf("The is integer: %i\n", '6');
 	ft_printf("The is unsigned decimal: %u\n", '6');
-	ft_printf("The is lower case hexa: %x\n", f);
-	ft_printf("The is upper case hexa: %X\n", f);
+	ft_printf("The is lower case hexa: %x\n", n);
+	ft_printf("The is upper case hexa: %X\n", n);
 	ft_printf("The is pointer: %p\n", p);
-	ft_printf("The is character: %c\n", 'f');
+	ft_printf("The is character: %c\n", 'n');
 	ft_printf("The is percentage: %%\n\n");
 }
